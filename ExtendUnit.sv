730ca4d147af19fa0484128b0fe2d834b9b94c72
akselsaatci <100696686+akselsaatci@users.noreply.github.com>
2025-01-13 19:44:19 +0300

    feat:implemented ExtendUnit

--- ExtendUnit.sv
+++ ExtendUnit.sv
@@ -20,7 +20,25 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module ExtendUnit(
-
-    );
+module ExtendUnit (
+    input  logic [23:0] inst,
+    input  logic [ 1:0] immSrc,
+    output logic [31:0] ExtImm
+);
+  always_comb begin
+    case (immSrc)
+      2'b00: begin
+        ExtImm = {24'b0, inst[7:0]};
+      end
+      2'b01: begin
+        ExtImm = {20'b0, Instr[11:0]};
+      end
+      2'b10: begin
+        ExtImm = {{6{Instr[23]}}, Instr[23:0], 2'b00};
+      end
+      default: begin
+        //Not supported
+      end
+    endcase
+  end
 endmodule
